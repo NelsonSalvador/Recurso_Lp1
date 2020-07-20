7b5ebfcde885f248f657681989d06fd9e3fe19c2
Vascord <duarte-vasco@outlook.com>
2020-07-20 18:42:01 +0100

    Start working on Simulator, need class world

--- Program/Simulator.cs
+++ Program/Simulator.cs
@@ -2,9 +2,41 @@ namespace Program
 {
     public class Simulator
     {
+        private int agents_alive;
+        private int turn = 0;
+ 
         public Simulator(int N, int M, int L, int T, int Tinf, bool view)
         {
-            //...
+            /*Proprieties importantes para gameloop:
+            agentes vivos,
+            turnos,
+            turno infetado.
+
+            Proprieties importantes para UI:
+            turnos,
+            agentes (todos os estados)
+            */
+            this.agents_alive = M;
+
+            GameLoop(view, T, Tinf);
+        }
+
+        public void GameLoop(bool view, int max_turn, int infection_turn)
+        {
+            //World world = new World(N, M, L);
+
+            while(true)
+            {
+                if(/*(world.agents_alive == 0) && */(turn == max_turn))
+                {
+                    //End the simulation, get the stats if the option is right
+                }
+                else
+                {
+                    //world.World
+                }
+
+            }
         }
     }
 }
